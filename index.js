d3de93708949a159f96610742c8a628dafdeefe7
shantel1111 <shantel.artch@gmail.com>
2025-01-10 14:28:37 +1000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,119 @@
+// 91 characters
+// generating two random passwords when user clicks the button
+// each password should be 15 characters long
+
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 15;
+
+function generateRandomCharacter() {
+  let getIndex = Math.floor(Math.random() * characters.length);
+  return characters[getIndex];
+}
+
+function generatePassword() {
+  let randomPassword = "";
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += generateRandomCharacter();
+  }
+  return randomPassword;
+}
+
+function passwordOptions() {
+  let optionOne = document.getElementById("optionOne-el");
+  let optionTwo = document.getElementById("optionTwo-el");
+  optionOne.textContent = generatePassword();
+  optionTwo.textContent = generatePassword();
+}
