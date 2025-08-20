d30e521753da8b1bf09ad8dcc77ff63078543d6a
shantel1111 <shantel.artch@gmail.com>
2025-08-20 14:07:49 -0400

    Updated value on document load

--- index.js
+++ index.js
@@ -1,110 +1,26 @@
-const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
 // const uppercaseCharacters =
 // const lowercaseCharacters =
-// const numbers =
+// const numbers = 
 // const symbols =
 
-let passwordLength = document.getElementById("password-length");
+let buttonEl = document.getElementById("button-el")
+let optionOne = document.getElementById("optionOne-el");
+let passwordLength = document.getElementById("password-length")
 let rangeInput = document.getElementById("range-input");
-let rangeValue = "";
+let currentValue = rangeInput.value
 
-rangeInput.addEventListener("input", function () {
-  rangeValue = rangeInput.value;
-  passwordLength.textContent = `Password Length: ${rangeInput.value}`;
-  console.log(rangeInput.value);
-});
+document.addEventListener("DOMContentLoaded", function(){
+   optionOne.textContent = generatePassword();
+})
+
+rangeInput.addEventListener("input", function(){
+  currentValue = rangeInput.value
+  passwordLength.textContent = `Password Length: ${rangeInput.value}`
+  console.log(rangeInput.value)
+})
 
 function generateRandomCharacter() {
   let getIndex = Math.floor(Math.random() * characters.length);
@@ -113,17 +29,13 @@ function generateRandomCharacter() {
 
 function generatePassword() {
   let randomPassword = "";
-  for (let i = 0; i < rangeValue; i++) {
+  for (let i = 0; i < currentValue; i++) {
     randomPassword += generateRandomCharacter();
   }
-  console.log(randomPassword);
+  console.log(randomPassword)
   return randomPassword;
 }
 
-let buttonEl = document.getElementById("button-el");
-let optionOne = document.getElementById("optionOne-el");
-
 buttonEl.addEventListener("click", function passwordOptions() {
   optionOne.textContent = generatePassword();
-});
-
+})
